96cda7b68f1d19ed49bc13e1b0e9e1b9ab8ff7ca
Mansi130 <76936399+Mansi130@users.noreply.github.com>
2021-06-28 12:23:45 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascripts and React.Js</h1>
+      <p>This is an amazing bootcamp taken by Shaurya Sinha sir</p>
+    </div>
+  );
+}
+
+export default Info;
